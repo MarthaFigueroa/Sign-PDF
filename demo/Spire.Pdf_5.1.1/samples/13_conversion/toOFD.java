4df0ddd094336ecc04701c428729194ab4f8f904
MarthaFigueroa <43876513+MarthaFigueroa@users.noreply.github.com>
2022-02-22 17:50:49 +0100

    Delete demo/Spire.Pdf_5.1.1 directory

--- demo/Spire.Pdf_5.1.1/samples/13_conversion/toOFD.java
+++
@@ -1,9 +0,0 @@
-import com.spire.pdf.*;
-
-public class toOFD {
-    public static void main(String[] args) {
-        PdfDocument pdfDocument =new PdfDocument();
-        pdfDocument.loadFromFile("data/Sample.pdf");
-        pdfDocument.saveToFile("output/toOFD.ofd",FileFormat.OFD);
-    }
-}
