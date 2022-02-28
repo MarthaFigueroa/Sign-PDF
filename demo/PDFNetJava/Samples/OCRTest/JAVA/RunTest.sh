50451e0b2d355d5218a921c4315676730bc7ec8e
martha.marquez <martha.marquez@alumnos.uneatlantico.es>
2022-02-28 17:04:47 +0100

    remove fail

--- demo/PDFNetJava/Samples/OCRTest/JAVA/RunTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-TEST_NAME=OCRTest
-javac -cp ../../../Lib/PDFNet.jar ../../LicenseKey/JAVA/PDFTronLicense.java *.java
-java -Djava.library.path=../../../Lib -cp .:../../../Lib/PDFNet.jar:../../LicenseKey/JAVA $TEST_NAME
